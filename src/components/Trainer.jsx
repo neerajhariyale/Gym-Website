ec11bfc43ff41d637c09b765e0a915c1d57333f6
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2025-02-14 08:42:39 +0530

    Update Trainer.jsx

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -6,7 +6,7 @@ const Trainer = () => {
       <p className="text-center mt-8 font-bold text-2xl sm:text-3xl">EXPERT TRAINER</p>
       <div className="w-16 border-2 border-yellow-500 mt-2"></div>
 
-      <div className="flex flex-col sm:flex-row gap-8 justify-center items-center mt-6 mb-8">
+      <div className="flex flex-col md:flex-row  gap-8 justify-center items-center mt-6 mb-8">
         <div className="flex flex-col h-72 w-full sm:w-64 gap-4 border-2 p-4 justify-center items-center rounded-lg cursor-pointer hover:shadow-2xl transition duration-300">
           <img 
             src="https://lh3.googleusercontent.com/pw/AP1GczMmVV5PfFzpv367grCArhQyRDiVzeE5ZmdNDe0Y3UNbcSh5uBTwIEZ6AjjU3jD6qni-nE-O24fqODhW9l3GetWMwLZVEIiu271Peq_qQRhNyA5KQtU85A1Rrs0uj1lXlkezg3fKnU7j0HnpmqFM4Fg=w716-h740-s-no-gm?authuser=0" 
