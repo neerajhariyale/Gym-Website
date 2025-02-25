63918fc0f296189d540c85953c32629bcaa12da4
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2025-02-25 10:04:53 +0530

    Update Registration.jsx

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import girl from "../assests/girl.jpg";
-import './Btn1.css';
+import "./Btn1.css";
 
 const Registration = () => {
   return (
-    <div className="border-none w-screen">
-      <div className='relative w-full '>
-        <p className='absolute mt-80 ml-[26rem] font-sans tracking-wide  text-5xl text-[#978a7d] '>BOOK YOUR APPOINTMENT</p>
-        <p className='absolute mt-96 ml-[26rem] font-sans tracking-wide text-5xl text-[#897969]'>BE READY FOR WORKOUT !</p>
-        <button className='absolute mt-[29rem]  font-sans tracking-wide text-xl text-[#f8dbbc] btn1 '>APPOINTMENT</button> 
-        <img src={girl} alt=""  />
+    <div className="relative w-full flex justify-center items-center">
+      {/* Background Image */}
+      <img src={girl} alt="Workout Girl" className="w-full h-auto object-cover" />
+
+      {/* Text Overlay */}
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center px-4">
+        <p className="text-3xl sm:text-5xl font-sans tracking-wide text-[#978a7d]">
+          BOOK YOUR APPOINTMENT
+        </p>
+        <p className="text-3xl sm:text-5xl font-sans tracking-wide text-[#897969] mt-4">
+          BE READY FOR WORKOUT!
+        </p>
+
+        {/* Appointment Button */}
+        <button className="mt-6 sm:mt-8 px-6 py-3 text-xl font-sans tracking-wide text-[#f8dbbc] btn1">
+          APPOINTMENT
+        </button>
       </div>
     </div>
   );
