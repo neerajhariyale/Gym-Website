6621dc65ccfdf9edab41c18d80bfa5e3d033df26
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2025-02-25 10:00:00 +0530

    Update Details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,24 +1,20 @@
-import React from 'react'
-import mainImg from "../assests/main.jpg"
+import React from 'react';
+import mainImg from "../assests/main.jpg";
 import './Btn.css';
 
 const Details = () => {
   return (
     <div className='border-none'>
-
-      <div className='relative '>
-        <p className='absolute mt-48 ml-48 font-sans tracking-wide text-5xl text-[#bb6d4c]' >SHAPE YOUR BODY</p>
-        <p className='absolute mt-64 ml-48 font-sans tracking-wide text-3xl text-[#88736e]'>BE STRONG TRAINING HARD</p>
-        <button className='absolute mt-80 ml-48 font-sans tracking-wide text-xl text-[#f8dbbc] btn '>GET INFO</button> 
-        <img src={mainImg} alt="" className='max-w-full'/>
+      <div className='relative w-full'>
+        <img src={mainImg} alt="" className='w-full h-auto' />
+        <div className='absolute top-1/4 left-1/2 transform -translate-x-1/2 text-center px-4 sm:px-8'>
+          <p className='font-sans tracking-wide text-3xl sm:text-5xl text-[#bb6d4c]'>SHAPE YOUR BODY</p>
+          <p className='mt-4 font-sans tracking-wide text-lg sm:text-3xl text-[#88736e]'>BE STRONG TRAIN HARD</p>
+          <button className='mt-6 font-sans tracking-wide text-lg sm:text-xl text-[#f8dbbc] btn px-6 py-2'>GET INFO</button>
+        </div>
       </div>
-
-      <div>
-        
-      </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
