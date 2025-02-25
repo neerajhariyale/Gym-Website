c7a1adfc97060a7e8d6e0d26c9bfb3b7d7bbe342
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2025-02-25 09:59:05 +0530

    Update Copyright.jsx

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from 'react';
 import { FcLike } from "react-icons/fc";
 
 const Copyright = () => {
   return (
-    <div className='w-11/12 flex justify-center items-center  mt-0 mb-4'>
-      <div className='flex  gap-1'>
-      Copyright ©2024 All rights reserved | This template is made with  <span className='flex justify-center items-center '> <FcLike /></span>  by  <span className='text-red-500'>Neeraj.</span> 
+    <div className='w-11/12 flex justify-center items-center mt-2 mb-4 text-center'>
+      <div className='flex flex-wrap justify-center items-center gap-1 text-sm sm:text-base px-2'>
+        Copyright ©2024 All rights reserved | This template is made with 
+        <span className='flex justify-center items-center'> <FcLike /></span> 
+        by <span className='text-red-500 font-semibold'>Neeraj.</span> 
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Copyright
+export default Copyright;
